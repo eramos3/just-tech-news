22c827a7554b655fa5fc0109f33f3b445639371e
eramos3 <cisco@Eders-MacBook-Pro.local>
2021-01-28 15:40:22 -0800

    fixed login issues

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     Comment.findAll({
@@ -24,7 +25,7 @@ router.get('/', (req, res) => {
 //             res.status(400).json(err);
 //         });
 // });
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     // check the session
     if (req.session) {
         Comment.create({
@@ -41,7 +42,7 @@ router.post('/', (req, res) => {
     }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Comment.destroy({
         where: { id: req.params.id }
     })
@@ -51,7 +52,10 @@ router.delete('/:id', (req, res) => {
                 return;
             }
             res.json(dbCommentData);
-        })
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
